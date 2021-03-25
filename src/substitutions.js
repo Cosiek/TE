5f472835bc3d2ba15bfecbddef54bfb085ad4d63
Cosiek <fitek2@o2.pl>
2021-03-26 00:00:05 +0100

    More improvements on substitutions

--- src/substitutions.js
+++ src/substitutions.js
@@ -1,4 +1,6 @@
 const REGEX = /(^|[^\\]){{([^=}]+)(=(.*?))?([^\\])}}/g
+const SIMPLER_REGEX = /(^|[^\\]){{([^=}]+)([^\\=])}}/g  // used for seconr iteration - where there are no
+                                                        // value definitions.
 
 function handleSubstitutions(text){
     // TODO: handle all-whitespace cases
@@ -7,9 +9,11 @@ function handleSubstitutions(text){
     // {{ token }} for insert
     let newText = "";
     let values = new Map();
-    for (let line of text.split('\n')){
-        let matchesIterator = line.matchAll(REGEX);
-        newLine = line;
+    let missingKeys = new Map();
+    let lines = text.split('\n');
+    for (let counter in lines){
+        newLine = lines[counter];
+        let matchesIterator = newLine.matchAll(REGEX);
         for (let match of matchesIterator){
             let key = match[2].trim();
             let val = ("" + match[4]).trim()
@@ -18,15 +22,58 @@ function handleSubstitutions(text){
                 val = val.trim()
                 val = val.replace(/\\(.)/g, "$1")
                 values.set(key, val);
+                if (missingKeys.get(key) === undefined){
+                    missingKeys.set(key, val);
+                }
             } else {
                 key += match[5];
                 key = key.trim()
             }
-            newLine = newLine.replace(match[0], match[1] + values.get(key));
+
+            if (values.get(key) === undefined){
+                // add this to missing keys
+                missingKeys.set(key, undefined);
+            } else {
+                newLine = newLine.replace(match[0], match[1] + values.get(key));
+            }
         }
-        newText += newLine + "\n"
+        lines[counter] = newLine;
     }
-    return newText;
+    return lines.join("\n") + "\n";  // TODO: get rid of trailing "\n"
+
+    // substitute values defined after use
+    for (let counter in lines){
+        newLine = lines[counter];
+        let matchesIterator = newLine.matchAll(SIMPLER_REGEX);
+        for (let match of matchesIterator){
+            let key = match[2].trim();
+            let val = ("" + match[4]).trim()
+            if (match[4] && val.length){
+                val += match[5]
+                val = val.trim()
+                val = val.replace(/\\(.)/g, "$1")
+                values.set(key, val);
+                if (missingKeys.get(key) === undefined){
+                    missingKeys.set(key, val);
+                }
+            } else {
+                key += match[5];
+                key = key.trim()
+            }
+
+            if (values.get(key) === undefined){
+                // add this to missing keys
+                missingKeys.set(key, undefined);
+            } else {
+                newLine = newLine.replace(match[0], match[1] + values.get(key));
+            }
+        }
+        lines[counter] = newLine;
+    }
+
+
+
+    return lines.join("\n") + "\n";  // TODO: get rid of trailing "\n"
 }
 
 module.exports.substitute = handleSubstitutions;
--- test/substitutions.test.js
+++ test/substitutions.test.js
@@ -7,6 +7,10 @@ test('Basic substitutions', () => {
     let sampleInput = "1 {{size=little}} kitty, 2 {{size}} kitties, 3 {{size}} kitties";
     let expected = "1 little kitty, 2 little kitties, 3 little kitties\n";
     expect(substitutions.substitute(sampleInput)).toBe(expected);
+    // single letter variable name
+    sampleInput = "1 {{s=little}} kitty, 2 {{s}} kitties, 3 {{s}} kitties";
+    expect(substitutions.substitute(sampleInput)).toBe(expected);
+    // single char variable value
 });
 
 test('Substitutions - proper escaping', () => {
@@ -24,13 +28,26 @@ test('Substitutions - proper escaping', () => {
     expect(substitutions.substitute("1 {{size=lit=tle}} kitty")).toBe("1 lit=tle kitty\n");
 });
 
-test('Substitutions - all-whitespace cases', () => {});
-test('Substitutions - definition after use', () => {});
-test('Substitutions - redefinition', () => {});
+test('Substitutions - all-whitespace cases', () => {
+    let sampleInput = "1 {{=little}} kitty, 2 {{}} kitties, 3 {{size=}} kitties, 4 {{size}} kitties";
+    let expected = "1 little kitty, 2 little kitties, 3 little kitties\n";
+    expect(substitutions.substitute(sampleInput)).toBe(expected);
+});
+
+test('Substitutions - redefinition', () => {
+    let sampleInput = "1 {{size=little}} kitty, 2 {{size}} kitties, 3 {{size=small}} kitties, 4 {{size}} kitties";
+    let expected = "1 little kitty, 2 little kitties, 3 small kitties, 4 small kitties\n";
+    expect(substitutions.substitute(sampleInput)).toBe(expected);
+});
+
 test('Substitutions - undeclared', () => {});
 
 test('Substitutions - definition after use', () => {
     let sampleInput = "1 {{size}} kitty, 2 {{size}} kitties, 3 {{size=little}} kitties";
     let expected = "1 little kitty, 2 little kitties, 3 little kitties\n";
+    expect(substitutions.substitute(sampleInput)).toBe(expected);
+    // same here, but multi-line
+    sampleInput = "1 {{size}} kitty,\n 2 {{size}} kitties,\n 3 {{size=little}} kitties";
+    expected = "1 little kitty,\n 2 little kitties,\n 3 little kitties\n";
     expect(substitutions.substitute(sampleInput)).toBe(expected);
 });
